3b61073013b2122e034c009cda76a388f16ea641
b21421 <yogeshparjapati46@gmail.com>
2025-12-28 17:06:47 +0530

    Added the date fiels in notes

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify
+from datetime import date as date_class
 
 app = Flask(__name__)
 
-# Temporary in-memory storage - data will be lost when server stops
 users = []
 notes = []
 
 
-# GET endpoint - View all notes
 @app.route("/notes/<int:user_id>", methods=["GET"])
 def get_user_notes(user_id):
     user_notes = [n for n in notes if n["user_id"] == user_id]
+
+    req_date = request.args.get("date")
+    if req_date:
+        user_notes = [n for n in user_notes if n["date"] == req_date]
 
     return jsonify({
         "user_id": user_id,
@@ -42,11 +45,12 @@ def create_user():
 @app.route("/notes", methods=["POST"])
 def create_note():
     data = request.json
+    print("Received Body:", data)     # DEBUG
+    print("Notes before:", notes)     # DEBUG
 
     if not data or "user_id" not in data:
         return jsonify({"error": "user_id is required"}), 400
 
-    # check user exists
     user = next((u for u in users if u["id"] == data["user_id"]), None)
     if not user:
         return jsonify({"error": "User not found"}), 404
@@ -55,10 +59,14 @@ def create_note():
         "id": len(notes) + 1,
         "user_id": data["user_id"],
         "title": data.get("title"),
-        "content": data.get("content")
+        "content": data.get("content"),
+        "date": data.get("date", str(date_class.today()))
     }
 
     notes.append(note)
+
+    print("Notes after:", notes)      # DEBUG
+    print("Note dictionary:", note)   # DEBUG - verify date is in note dict
 
     return jsonify({
         "message": "Note created successfully",
